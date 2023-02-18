fa39bf296cb1b799b8fcb1b2d8e8a973bf3015ba
С Г <guplik@bk.ru>
2023-02-18 11:36:58 +0300

    g

--- Assets/Scripts/moveCamera.cs
+++ Assets/Scripts/moveCamera.cs
@@ -5,27 +5,27 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class moveCamera : MonoBehaviour
 {
-    [SerializeField] private GameObject _player;
+    //[SerializeField] private GameObject _player;
 
     private Transform _transfrom;
 
     private void Start()
     {
         //GameObject g =  GameObject.FindFirstObjectByType<MovePlayer>();
-        _player = GameObject.FindGameObjectWithTag("Player");
+        //_player = GameObject.FindGameObjectWithTag("Player");
         _transfrom = GetComponent<Transform>();
     }
 
    
-    private void Update()
-    {
-        if(_player == null)
-        {
-            _player = GameObject.FindGameObjectWithTag("Player");
-        }
+    //private void Update()
+    //{
+    //    if(_player == null)
+    //    {
+    //        _player = GameObject.FindGameObjectWithTag("Player");
+    //    }
             
-        _transfrom.position = new Vector3(_player.transform.position.x, _player.transform.position.y, -10);
-    }
+    //    _transfrom.position = new Vector3(_player.transform.position.x, _player.transform.position.y, -10);
+    //}
 
 
 
