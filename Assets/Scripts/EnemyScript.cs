a610b556ccb551e99320b27e13490c739714302c
С Г <guplik@bk.ru>
2023-02-21 21:45:26 +0300

    Начата работа над агром врагов

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -6,25 +6,36 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    [SerializeField] private GameObject _prefabBullet;
-    [SerializeField] private GameObject _spawnPointBullet;
+    [SerializeField] public GameObject _prefabBullet;
+    [SerializeField] public GameObject _spawnPointBullet;
+    [SerializeField] public GameObject _checkPlayerPoint;
 
+    [SerializeField] public float _speed = 1;
+    
     [SerializeField] private float leftFlagPositon;
     [SerializeField] private float rightFlagPositon;
 
-    [SerializeField] public float _speed = 1;
 
     [SerializeField] private bool _isLeft;
 
     private Transform _transform;
+    private GameObject _player;
 
     private void Start()
     {
         _transform = GetComponent<Transform>();
+        _player = GameObject.FindGameObjectWithTag("Player");
     }
     private void Update()
     {
-        
+        Debug.Log("p" + _player.transform.localPosition.x);
+        Debug.Log( "c" + _checkPlayerPoint.transform.position.x);;
+ 
+        if (_checkPlayerPoint.transform.localPosition.x < _player.transform.localPosition.x)
+        {
+            Debug.Log("1");
+        }
+
         if (_isLeft)
         {
             transform.position -= Vector3.left * _speed * Time.deltaTime;
